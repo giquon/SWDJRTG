be59602342f84155d73574ea06fed1291d5aa632
H4rmey <hammiekaaz@gmail.com>
2020-06-27 01:24:36 +0200

    what does this do?

---
+++ Assets/Scripts/GrounCheck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrounCheck : MonoBehaviour
+{
+    private GrounCheck gc;
+    public bool isGrounded = false;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        gc = GetComponent<GrounCheck>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Platform")
+        {
+            isGrounded = true; ;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.transform.tag == "Platform")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Platform")
+        {
+            isGrounded = false;
+        }
+    }
+}
