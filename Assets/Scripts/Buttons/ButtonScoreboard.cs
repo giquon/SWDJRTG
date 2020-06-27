a30c2c8fd0e1bdcd48c6af60110bf931f5acb215
H4rmey <hammiekaaz@gmail.com>
2020-06-27 20:14:40 +0200

    started menu fixes

---
+++ Assets/Scripts/Buttons/ButtonScoreboard.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScoreboard : MonoBehaviour
+{
+    public void GotoScoreboard()
+    {
+        SceneManager.LoadScene("Scoreboard", LoadSceneMode.Single);
+    }
+}
