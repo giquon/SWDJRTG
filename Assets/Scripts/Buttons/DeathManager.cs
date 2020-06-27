be6b23001cd6dfaf89f24943c4c669c3979a17e0
H4rmey <hammiekaaz@gmail.com>
2020-06-28 00:52:12 +0200

    updated scores

--- Assets/Scripts/Buttons/DeathManager.cs
+++ Assets/Scripts/Buttons/DeathManager.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DeathManager : MonoBehaviour
 {
+    public Text score;
+
+    private void Awake()
+    {
+        score.text = "Your Score: " + PlayerPrefs.GetInt("tempscore");
+    }
+
     public void Sumbit()
     {
         //handle submissions here :)
--- Assets/Scripts/Buttons/ScoreboardManager.cs
+++ Assets/Scripts/Buttons/ScoreboardManager.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ScoreboardManager : MonoBehaviour
 {
+    public Text score;
+
+    private void Awake()
+    {
+        score.text = "Highscore: " + PlayerPrefs.GetInt("Highscore");
+    }
+
     public void GotoPlayGame()
     {
         SceneManager.LoadScene("Game", LoadSceneMode.Single);
