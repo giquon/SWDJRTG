94b103b9ca0808dddf79679d99b5f3d77a9cac4d
H4rmey <hammiekaaz@gmail.com>
2020-06-27 21:34:40 +0200

    menu transitions are complete

---
+++ Assets/Scripts/Buttons/MainMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void GotoPlayGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void GotoScoreboard()
+    {
+        SceneManager.LoadScene("Scoreboard", LoadSceneMode.Single);
+    }
+
+    public void GotoExit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Buttons/ScoreboardManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreboardManager : MonoBehaviour
+{
+    public void GotoPlayGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
