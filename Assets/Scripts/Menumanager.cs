664dd6485aaa71f72e89706a1fd9769d41c7c1dc
H4rmey <hammiekaaz@gmail.com>
2020-06-27 16:26:04 +0200

    added menu things

---
+++ Assets/Scripts/Menumanager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menumanager : MonoBehaviour
+{
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void GotoDeathscreen()
+    {
+        SceneManager.LoadScene("Deathscreen", LoadSceneMode.Single);
+    }
+
+    public void GotoScoreboard()
+    {
+        SceneManager.LoadScene("Scoreboard", LoadSceneMode.Single);
+    }
+
+    public void GotoGame()
+    {
+        SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
