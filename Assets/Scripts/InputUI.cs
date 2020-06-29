4e223fd11e4ea27877248a1502bd30ce2dded97d
H4rmey <hammiekaaz@gmail.com>
2020-06-29 11:48:42 +0200

    Update - small changes

--- Assets/Scripts/InputUI.cs
+++ Assets/Scripts/InputUI.cs
@@ -13,21 +13,29 @@ public class InputUI : MonoBehaviour
     private Image moveImageRegion;
 
     // Use this for initialization
-    void Start()
+    void Awake()
     {
         inputHandler = GetComponent<InputHandler>();
 
         //get image component
         moveImageRegion = moveRegion.GetComponent<Image>();
         jumpImageRegion = jumpRegion.GetComponent<Image>();
+
+        //render the move image and scale it to size
+        moveImageRegion.rectTransform.sizeDelta     = new Vector2(Screen.width, inputHandler.yMoveLimit);
+        moveRegion.transform.position               = new Vector3(Screen.width/2, inputHandler.yMoveLimit/2, 0);
+
+        //render the jump image and scale it to size
+        jumpImageRegion.rectTransform.sizeDelta     = new Vector2(Screen.width, inputHandler.yJumpSize);
+        jumpRegion.transform.position               = new Vector3(Screen.width/2, inputHandler.yMoveLimit + inputHandler.yJumpSize/2, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         //render the move image and scale it to size
-        moveImageRegion.rectTransform.sizeDelta = new Vector2(Screen.width, inputHandler.yMoveLimit);
-        moveRegion.transform.position           = new Vector3(Screen.width / 2, inputHandler.yMoveLimit / 2, 0);
+        moveImageRegion.rectTransform.sizeDelta     = new Vector2(Screen.width, inputHandler.yMoveLimit);
+        moveRegion.transform.position               = new Vector3(Screen.width / 2, inputHandler.yMoveLimit / 2, 0);
 
         //render the jump image and scale it to size
         jumpImageRegion.rectTransform.sizeDelta = new Vector2(Screen.width, inputHandler.yJumpSize);
